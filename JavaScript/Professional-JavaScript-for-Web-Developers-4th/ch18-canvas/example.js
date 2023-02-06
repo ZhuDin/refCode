d8713e752a0c4415645f60cdd80921372c55b996
ZhuDin <279388213@qq.com>
2023-02-06 15:46:18 +0800

    js canvas

--- JavaScript/Professional-JavaScript-for-Web-Developers-4th/ch18-canvas/example.js
+++ JavaScript/Professional-JavaScript-for-Web-Developers-4th/ch18-canvas/example.js
@@ -10,6 +10,21 @@ if (drawing.getContext) {
     context.strokeRect(30, 30, 50, 50)
     context.clearRect(40, 40, 10, 10)
 
+    context.beginPath();
+    context.arc(200, 200, 99, 0, (2 * Math.PI), false)
+    context.moveTo(294, 200)
+    context.arc(200, 200, 94, 0, (2 * Math.PI), false)
+    context.moveTo(200, 200)
+    context.lineTo(200, 130)
+    context.moveTo(200, 200)
+    context.lineTo(150, 200)
+    context.stroke()
+
+    context.font = 'bold 14px console'
+    context.textAlign = 'center'
+    context.textBaseline = 'middle'
+    context.fillText('12', 200, 120)
+
     let imgURI = drawing.toDataURL("image/jpeg")
     let image = document.createElement('img')
     image.src = imgURI
