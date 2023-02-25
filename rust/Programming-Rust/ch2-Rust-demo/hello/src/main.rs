a3b2f4dbfc1f04872afbaa17b33ee1835e5c4375
ZhuDin <279388213@qq.com>
2023-02-25 23:01:41 +0800

    rust demo init

---
+++ rust/Programming-Rust/ch2-Rust-demo/hello/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("cargo new hello");
+    println!("cargo run");
+    println!("cargo clean");
+    println!("Hello, world!");
+    println!("cargo --version");
+    println!("rustc --version");
+    println!("rustdoc --version");
+}
